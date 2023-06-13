12fef178a3316d8edf817ab0d475f820434f61e8
Renato Suares <renatorsc2006@gmail.com>
2023-06-13 13:29:57 -0400

    início da revisão

---
+++ WindowsFormsApp1/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class menu : Form
+    {
+        public menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_divisao(object sender, EventArgs e)
+        {
+            Divisao dv = new Divisao();
+            dv.ShowDialog();
+        }
+    }
+}
